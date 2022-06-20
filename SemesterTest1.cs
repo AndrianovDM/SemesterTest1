32a5185b69dfcf033f176ec6c93994c6af8db28c
Dmitry <andrianovdm1997@mail.ru>
2022-06-20 18:34:09 +0300

    add PrintArray

--- SemesterTest1.cs
+++ SemesterTest1.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] arr)
+{   
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {   
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine("]");
+}
